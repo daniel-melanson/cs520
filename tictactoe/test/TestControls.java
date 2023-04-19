c20de6c6408020b25f60ee16b6527d978b140041
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2023-04-19 18:51:48 -0400

    test: legal undo

--- tictactoe/test/TestControls.java
+++ tictactoe/test/TestControls.java
@@ -1,8 +1,12 @@
+import static org.junit.Assert.assertEquals;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import controller.RowGameController;
+import model.Player;
+import view.BlockIndex;
 
 public class TestControls {
     private RowGameController game;
@@ -32,5 +36,15 @@ public class TestControls {
     public void testLegalUndo() {
         // 7. If the user has done at least one move, the user is permitted to undo and
         // the game is updated appropriately.
+        game.move(new BlockIndex(0, 0));
+
+        game.undoMove();
+
+        assertEquals(Player.PLAYER_1, game.gameModel.getPlayer());
+        assertEquals(9, game.gameModel.movesLeft);
+        assertEquals(game.gameModel.movesMade.empty(), true);
+        
+        assertEquals(game.gameModel.blocksData[0][0].getIsLegalMove(), true);
+        assertEquals(game.gameModel.blocksData[0][0].getContents(), "");
     }
 }
